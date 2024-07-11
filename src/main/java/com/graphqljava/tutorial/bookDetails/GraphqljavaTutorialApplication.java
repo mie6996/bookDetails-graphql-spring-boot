ca4c7d97dd7a0d3e920a080f05a31b135f9caab1
mie6996 <nguyenxuanphat060301@gmail.com>
2024-07-11 16:21:02 +0700

    GraphQL in spring boot

---
+++ src/main/java/com/graphqljava/tutorial/bookDetails/GraphqljavaTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.graphqljava.tutorial.bookDetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqljavaTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqljavaTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/graphqljava/tutorial/bookDetails/GraphqljavaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphqljava.tutorial.bookDetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqljavaTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
